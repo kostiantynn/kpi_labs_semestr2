058792623ee7f651fb6f6944d7e6602a18ca347a
kostiantynn <kostya.code@gmail.com>
2020-03-03 22:42:42 +0200

    Realisation of string and abstract-class Text

---
+++ Laba2/MyString.cs
@@ -0,0 +1,13 @@
+﻿namespace Laba2
+{
+    public class MyString
+    {
+        public char[] Source { get; set; }
+        public int Length { get => Source.Length; }
+
+        public MyString(string str)
+        {
+            this.Source = str.ToCharArray();
+        }
+    }
+}
