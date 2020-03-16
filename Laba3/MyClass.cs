4711f98d5b913f61ce947c922a2556d44332890c
kostiantynn <kostya.code@gmail.com>
2020-03-16 12:06:27 +0200

    Creating possible solution for kpi-laba 3

---
+++ Laba3/MyClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Laba3
+{
+    class MyClass
+    {
+        public int SumOfElements { get => SumOfMatrixElements(); }
+        public int [,] Matrix { get; set; }
+        public int NumberOfRows { get; }
+        public int NumberOfColumns { get; }
+
+        public MyClass(int NumberOfRows, int NumberOfColumns)
+        {
+            this.NumberOfRows = NumberOfRows;
+            this.NumberOfColumns = NumberOfRows;
+            this.Matrix = new int[NumberOfRows, NumberOfColumns];
+
+        }
+
+         private int SumOfMatrixElements()
+        {
+            int sum = 0;
+            foreach (var elem in this.Matrix)
+                sum += elem;
+            return sum;
+        }
+        private float CalculateRMSValue(int index)
+        {
+            double rmsValue = 0.0;
+            for(int i = 0; i < this.NumberOfColumns; i++)
+                rmsValue += Math.Pow((double)this.Matrix[index, i], 2);
+            return (float)Math.Sqrt(rmsValue);
+        }
+        
+        public float this[int index] { get => CalculateRMSValue(index);  }
+    }
+}
---
+++ Laba3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix = new MyClass(2, 3) { Matrix = new int[2, 3] { {1, 2, 3 }, {2, 3, 1 } }  };
+            Console.WriteLine(matrix[1]);
+            Console.WriteLine(matrix.SumOfElements);
+        }
+    }
+}
