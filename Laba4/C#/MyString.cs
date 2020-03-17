85a520e6ebb9638354411cf7346c6b66a4b88940
kostiantynn <kostya.code@gmail.com>
2020-03-17 19:06:24 +0200

    Realisation of C#

---
+++ Laba4/C#/MyString.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace laba4
+{
+    public class MyString
+    {
+        private char[] StrValue { get; set; }
+
+        // Default constructor
+        public MyString()
+        {
+            this.StrValue = null;
+        } 
+        
+        // Constructor with params
+        public MyString(params char[] arrOfInteger)
+        {
+            this.StrValue = arrOfInteger;
+        }
+        
+        // Constructor of copying from another MyString
+        public MyString(MyString str)
+        {
+            this.StrValue = str.StrValue;
+        }
+
+        // Getting length of private char field
+        public int GetStringLength() => this.StrValue.Length;
+        
+        // Getting source of private char field
+        public void DisplaySource() => Console.WriteLine(this.StrValue);
+
+        // Writing indexer to access elements easily 
+        public int this[int index] {
+            get => this.StrValue[index];
+        }
+        
+        // Sum of two MyString
+        public static MyString operator +(MyString str1, MyString str2)
+        {
+            int counter = 0;
+            var str3 = new MyString();
+            str3.StrValue = new char[str1.GetStringLength() + str2.GetStringLength()];
+            
+            for (int i = 0; i < str1.GetStringLength(); i++)
+            {
+                str3.StrValue[counter] = str1.StrValue[i];
+                counter++;
+            }
+            for (int i = 0; i < str2.GetStringLength(); i++)
+            {
+                str3.StrValue[counter] = str2.StrValue[i];
+                counter++;
+            }
+            
+            return str3;
+        }
+
+        // Subtraction of two MyString objects
+        public static MyString operator -(MyString str1, char str2)
+        {
+            var str3 = new MyString();
+            str3.StrValue = new char[str1.GetStringLength()-1];
+            int counter = 0;
+            if (str1.StrValue.Contains(str2))
+            {
+                for (int i = 0; i < str1.GetStringLength(); i++)
+                {
+                    if (str2 == str1.StrValue[i]) {continue;}
+                    str3.StrValue[counter] = str1.StrValue[i];
+                    counter++;
+                }
+                
+                return str3;
+            }
+            throw new InvalidDataException($"Char {str2} does not exist in MyString object.");
+        }
+    }
+}
---
+++ Laba4/C#/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var C1 = new MyString();
+            var C2 = new MyString('1', '2', '3', '4', '5');
+            C2.DisplaySource();
+            var C3 = new MyString(C2);
+            C3.DisplaySource();
+            
+            C2 -= '5';
+            C2.DisplaySource();
+            
+            C1 = C2 + C3;
+            C1.DisplaySource();
+
+        }
+    }
+}
