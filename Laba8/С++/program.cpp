11be9ef6ac67292ec8eb9be013902642e765f3c9
kostiantynn <kostya.code@gmail.com>
2020-05-15 23:42:58 +0300

    Realisation of Lab8 in C++

---
+++ Laba8/С++/program.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int GetNumberOfDigitsInString(string str);
+
+int main() {
+	int (*GetNumberOfDigitsInStringPointer)(string);
+	GetNumberOfDigitsInStringPointer = GetNumberOfDigitsInString;
+	cout << GetNumberOfDigitsInStringPointer("XAE-12") << endl;
+	return 0;
+}
+
+int GetNumberOfDigitsInString(string str) {
+	int k = 0;
+	for (int i = 0; i < str.length(); i++) {
+		if (isdigit(str[i])) {
+			k++;
+		}
+	}
+	return k;
+}
