bbca5cba5656be2d163f3b86dda43c8648ab0f0a
kostiantynn <kostya.code@gmail.com>
2020-05-18 00:10:45 +0300

    Realisation of Laba7 in C#

---
+++ Laba7/C#/LinkedList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Laba7
+{
+    public class LinkedList
+    {
+        public Node Head;
+        
+        public class Node
+        {
+            public double Data { get; }
+            public Node Link;
+
+            public Node(double data)
+            {
+                Data = data;
+                Link = null;
+                
+            }
+        }
+
+        public int FindNumberOfElementsGreaterThanMedian()
+        {
+            var count = 0;
+            var median = MedianValue();
+            var temp = Head;
+            while (temp != null)
+            {
+                if (temp.Data > median)
+                {
+                    count++;
+                }
+
+                temp = temp.Link;
+            }
+
+            return count;
+        }
+
+        public void DeleteEvenElements()
+        {
+            var next = Head.Link;
+            var after = Head.Link.Link;
+            Head = next;
+            while (next != null && after != null)
+            {
+                next.Link = after.Link;
+                after = null;
+                next = next.Link;
+                if (next != null)
+                {
+                    after = next.Link;
+                }
+            }
+        }
+
+        public void PrintList()
+        {
+            var temp = Head;
+            while (temp != null)
+            {
+                Console.Write(temp.Data+ "->");
+                temp = temp.Link;
+            }
+
+            Console.WriteLine("null");
+        }
+        private double MedianValue()
+        {
+            var sum = 0.0;
+            var count = 0;
+            var temp = Head;
+            while (temp != null)
+            {
+                sum += temp.Data;
+                count++;
+                temp = temp.Link;
+            }
+
+            return sum / count;
+        }
+    }
+}
---
+++ Laba7/C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static Laba7.LinkedList;
+
+namespace Laba7
+{
+    class Program
+    {
+        static void Main()
+        {
+            var linkedlist = new LinkedList();
+            linkedlist.Head = new Node(1);
+            var node2 = new Node(2);
+            linkedlist.Head.Link = node2;
+            var node3 = new Node(3);
+            node2.Link = node3;
+            var node4 = new Node(4);
+            node3.Link = node4;
+            linkedlist.PrintList();
+            Console.WriteLine(linkedlist.FindNumberOfElementsGreaterThanMedian());
+            linkedlist.DeleteEvenElements();
+            linkedlist.PrintList();
+        }
+    }
+}
